636086d6f87e8132d704dbaa46f101b8d1f06383
SonuKumar123098 <sonukrsingh998@gmail.com>
2023-08-13 15:19:15 +0530

    run time overloading

---
+++ src/main/java/com/driver/A.java
@@ -0,0 +1,8 @@
+package com.driver;
+
+public class A {
+    String s="hello";
+    public String meth(){
+        return this.s;
+    }
+}
---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,8 @@
+package com.driver;
+
+public class B extends A {
+   String name="kallu";
+    public String meth(){
+        return name;
+    }
+}
