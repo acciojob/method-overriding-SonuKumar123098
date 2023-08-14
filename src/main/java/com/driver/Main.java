6a212e69b038f429010a49b5e944c774816cca89
SonuKumar123098 <sonukrsingh998@gmail.com>
2023-08-14 12:47:16 +0530

    solution1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,10 +2,10 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        A a=new A();
-        String meth = a.meth();
-        System.out.println(meth);
         B b=new B();
+        String meth = b.meth();
+        System.out.println(meth);
+//        B b=new B();
         System.out.println(b.meth());
     }
 }
